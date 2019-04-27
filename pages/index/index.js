91aaceb037a9408907932bb107f7e14ceeebbb32
home <869490494@qq.com>
2019-04-27 22:28:43 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,57 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+    data: {
+        imgUrls: [
+            '../../image/banner.png',
+            '../../image/banner.png',
+            '../../image/banner.png',
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: false,
+        circular: false,
+        interval: 2000,
+        duration: 500,
+        previousMargin: 0,
+        nextMargin: 0
+    },
+    changeProperty:
+        function (e){
+            var propertyName = e.currentTarget.dataset.propertyName
+            var newData = {}
+            newData[propertyName] = e.detail.value
+            this.setData(newData)
+        }
+    ,
+    changeIndicatorDots: function (e){
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    }
+    ,
+    changeAutoplay: function (e){
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    }
+    ,
+    intervalChange: function (e){
+        this.setData({
+            interval: e.detail.value
+        })
+    }
+    ,
+    durationChange: function (e){
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+    goDetail: function (){
+        wx.navigateTo({
+            url: '../tldetail/tldetail' + "?id=" + Math.floor(Math.random() * 100)
+        })
+    }
+})
+
